c8c45c72d367f9723a4f103ced6caf0c8588be74
gpunit2417 <punitgoyal106@gmail.com>
2025-03-22 12:07:46 +0530

    displayed desired no of rows

--- 01_pandas_basic/readCSVFile.py
+++ 01_pandas_basic/readCSVFile.py
@@ -2,3 +2,7 @@ import pandas as pd
 
 fileData = pd.read_csv("C:\\Users\\HP\\Desktop\\data.csv")
 print(fileData)
+print("*********************")
+print(fileData.head())
+print("*********************")
+print(fileData.head(7))
