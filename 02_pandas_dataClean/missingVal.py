76b2105b05dd008c21b113bd627fc71ce8497703
gpunit2417 <punitgoyal106@gmail.com>
2025-03-22 12:38:55 +0530

    tripped off the empty strings and NaN value from name and age respectively

--- 02_pandas_dataClean/missingVal.py
+++ 02_pandas_dataClean/missingVal.py
@@ -1,8 +1,10 @@
 import pandas as ps
-df = ps.DataFrame({'Name': ['punit', 'aastha', ''], 'age': [22, 21, 23]})
+df = ps.DataFrame({'Name': ['punit', 'goyal', ''], 'age': [22, None, 23]})
 print(df.head().to_string())
 
 # dropna() function check for a NaN value.. if present it will remove the entire row
 # data = df.dropna()
 # print(data)
 
+data = df[(df['Name'] != '') & (df['age'].notna())]
+print(data)
