261b4bf047aa453f75a4beb2a968f62f2fb1e341
gpunit2417 <punitgoyal106@gmail.com>
2025-03-22 12:26:04 +0530

    added null values to the dataframe

--- 01_pandas_basic/writeCSVFile.py
+++ 01_pandas_basic/writeCSVFile.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.DataFrame({'Name': ['Punit', 'Ajay', 'Sumit'], 'Age': [23, 25, 21], 'Passed': ['No', 'Yes', 'No']})
+df = pd.DataFrame({'Name': ['Punit', '', 'Sumit'], 'Age': [23, 25, None], 'Passed': ['No', 'Yes', 'No']})
 
 writeToCSVFile = df.to_csv("C:\\Users\\HP\\Desktop\\data1.csv", index = False)
 
