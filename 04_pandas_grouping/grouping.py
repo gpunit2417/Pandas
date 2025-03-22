3ec70cfa4a3c1e76c68090e4adece09edf66ecd2
gpunit2417 <punitgoyal106@gmail.com>
2025-03-22 15:50:55 +0530

    data grouping using groupby() function

--- 04_pandas_grouping/grouping.py
+++ 04_pandas_grouping/grouping.py
@@ -9,24 +9,29 @@ df = p.DataFrame({
 print(df.to_string())
 print("************************\n")
 
-df2 = p.DataFrame({
-    'EmpId' : [123, 134, 135, 145, 156],
-    'EmpName': ['Rohan', 'Surender', 'Aradhana', 'Monika', 'Punit'],
-    "Married": ["yes", 'yes', 'no', 'no', 'no']
-})
+# df2 = p.DataFrame({
+#     'EmpId' : [123, 134, 135, 145, 156],
+#     'EmpName': ['Rohan', 'Surender', 'Aradhana', 'Monika', 'Punit'],
+#     "Married": ["yes", 'yes', 'no', 'no', 'no']
+# })
 
-mergedData = p.merge(df, df2, how='cross')
-mergedData1 = p.merge(df, df2, how='left', on='EmpId')
-mergedData2 = p.merge(df, df2, how='right', on='EmpId')
-mergedData3 = p.merge(df, df2, how='outer', on='EmpId')
-mergedData4 = p.merge(df, df2, how='inner', on='EmpId')
-print(mergedData)
-print("************************\n")
-print(mergedData1)
-print("************************\n")
-print(mergedData2)
-print("************************\n")
-print(mergedData3)
-print("************************\n")
-print(mergedData4)
-print("************************\n")
+# mergedData = p.merge(df, df2, how='cross')
+# mergedData1 = p.merge(df, df2, how='left', on='EmpId')
+# mergedData2 = p.merge(df, df2, how='right', on='EmpId')
+# mergedData3 = p.merge(df, df2, how='outer', on='EmpId')
+# mergedData4 = p.merge(df, df2, how='inner', on='EmpId')
+# print(mergedData)
+# print("************************\n")
+# print(mergedData1)
+# print("************************\n")
+# print(mergedData2)
+# print("************************\n")
+# print(mergedData3)
+# print("************************\n")
+# print(mergedData4)
+# print("************************\n")
+
+# data grouping
+# groupedData = df.groupby("Department")["Salary"].mean()
+groupedData = df.groupby("Department")["Salary"].median()
+print(groupedData)
