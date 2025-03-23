3d5eae7c9bea9fcd380707ed1ea6071d33f41bbd
gpunit2417 <punitgoyal106@gmail.com>
2025-03-23 16:04:37 +0530

    write to csv file removing NA values

--- 01_pandas_basic/writeCSVFile.py
+++ 01_pandas_basic/writeCSVFile.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.DataFrame({'Name': ['Punit', '', 'Sumit'], 'Age': [23, 25, None], 'Passed': ['No', 'Yes', 'No']})
+df = pd.DataFrame({'Name': ['Punit', 'Rishabh', 'Sumit'], 'Age': [23, 25, 23], 'Passed': ['No', 'Yes', 'No']})
 
 writeToCSVFile = df.to_csv("C:\\Users\\HP\\Desktop\\data1.csv", index = False)
 
